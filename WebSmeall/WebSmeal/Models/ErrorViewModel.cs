9d145d7b7fd3fd2bb6f9bb0e9414ebaea350f8d3
wippi1511 <wippi1511@gmail.com>
2025-07-15 17:54:15 -0500

    Delete WebSmeall/WebSmeal directory

--- WebSmeall/WebSmeal/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebSmeal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
